981624ab66639d3f1e5118ca870bc4fd3c0c18f7
Herbert Lange <sonofchaos@gmx.de>
2017-08-22 14:26:31 +0200

    haskell source to enable tweeting

---
+++ GF-loves-tautologies.hs
@@ -0,0 +1,41 @@
+import PGF
+import System.Random
+import System.IO
+import Data.Time.Clock.POSIX
+import System.CPUTime
+import Twitter
+import Network.HTTP.Client
+import Data.ByteString.Lazy.Char8 hiding (putStrLn, length, head)
+    
+selectRandom :: RandomGen g => [a] -> g -> (a, g)
+selectRandom list rnd =
+  let
+    (rndNum,newRnd) = randomR (0,(length list - 1)) rnd
+    elem = list!!rndNum
+  in
+    (elem, newRnd)
+
+generate :: RandomGen g => PGF -> g -> (String,g)
+generate pgf rnd =
+  let
+    gen = generateRandomDepth rnd pgf (startCat pgf) (Just 4)
+    (lang,newRnd) = (selectRandom (languages pgf) rnd)
+    s = linearize pgf lang (head gen)
+    bind [] = []
+    bind (' ':'&':'+':' ':rest) = bind rest
+    bind (c:rest) = c:(bind rest)
+  in
+    (bind s,newRnd)
+
+main =
+  do
+    pgf <- readPGF "gf/Tautology.pgf"
+    conf <- configFromFile "config.json"
+    time <- round `fmap` getPOSIXTime
+    cpuptime <- getCPUTime
+    let cputime = cpuptime `div` 100000000
+    let rnd = mkStdGen (fromInteger (toInteger (time + cputime) ) )
+    let (s,newRnd) = generate pgf rnd
+
+    putStrLn s
+    either putStrLn (\c -> tweet c s >>= putStrLn . unpack . responseBody >> return () ) conf
